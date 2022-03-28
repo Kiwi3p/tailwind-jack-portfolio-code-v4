596c29dc83d0d58c4b273a33a32085705682a039
Jack Transue <johnktransue@gmail.com>
2022-03-27 21:59:40 -0400

    build fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react";
 import Game from "../components/Game";
 // import Model from "../components/Model";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Links from "../components/Links";
 // import { animationOne, transition } from "./animation";
 
